87a2b73b64af80845736211dcd69dd83bb4138f9
Dean <89344977+TheFighter82@users.noreply.github.com>
2025-07-15 12:50:48 -0400

    Added dash

--- Assets/Player_movement.cs
+++ Assets/Player_movement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_movement : MonoBehaviour
@@ -8,27 +10,53 @@ public class Player_movement : MonoBehaviour
     public LayerMask groundLayer;
     public Transform RaycastRotater;
     public Transform RaycastPoint;
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.2f;
+    public float DashDecelerator = 0.5f;
+    public bool isDashing;
+
+    Vector3 DashDirection;
+    IEnumerator Dash()
+    {
+        print("Dashing");
+        DashDecelerator = dashSpeed;
+        isDashing = true;
+        DashDirection = RaycastRotater.forward.normalized;
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+    }
     void FixedUpdate()
     {
         RaycastHit hit;
         Physics.Raycast(RaycastPoint.position, Vector3.down, out hit, 1f, groundLayer);
-        
+
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
         if (hit.collider != null)
         {
             print("On Ground");
-        transform.Translate(movement, Space.World); 
+            transform.Translate(movement, Space.World);
         }
 
-        
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isDashing)
+        {
+            
+            StartCoroutine(Dash());
+
+        }
 
         if (movement != Vector3.zero)
         {
             //transform.forward = moveme nt.normalized;
             RaycastRotater.forward = movement.normalized;
         }
+        if (isDashing && hit.collider != null)
+        {
+            DashDecelerator -= 0.5f;
+            transform.Translate(DashDirection * DashDecelerator * Time.deltaTime, Space.World);
+        }
 
     }
+    
 }
